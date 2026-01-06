4af1129efe5d36702f042b03b6076f8d4d51e94a
vandana <vandanareddem685@gmail.com>
2026-01-06 11:29:22 +0530

    variables

---
+++ shell-scripting/04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2:Hi $PERSON1,a very Good Moring"
+echo "$PERSON1:How are you $PERSON2"
+echo "$PERSON2:I Am good $PERSON1"
