aae5f91bea64986ce276f95a2c76909ed2e95975
vandana <vandanareddem685@gmail.com>
2026-01-08 18:13:48 +0530

    install mysql

--- shell-scripting/10-install-mssql.sh
+++ shell-scripting/10-install-mssql.sh
@@ -5,8 +5,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:please run this script with root user"
+    exit 1 #you can give other than 0
 else
-    echo "Yo are root user"
-fi
+    echo "You are root user"
+fi #fi means reverse of if , indicationg condition to end
 
 yum install mysql -y
