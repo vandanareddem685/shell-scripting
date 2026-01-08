c1abbbc365bc6e900545a5817f04920d0524e3c7
vandana <vandanareddem685@gmail.com>
2026-01-08 19:58:02 +0530

    Logs

--- shell-scripting/12-Logs.sh
+++ shell-scripting/12-Logs.sh
@@ -2,8 +2,8 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-RED="\e31m"
-GREEN="\e[32m"
+R="\e[31m"
+G="\e[32m"
 N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
@@ -11,20 +11,20 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
     if [ $1 -ne 0 ]
 then  
-    echo -e "$2.. $RED failed $N"
+    echo -e "$2.. $R failed $N"
     exit 1
 else
-    echo "$2  is $GREEN success $N"
+    echo -e "$2  is $G success $N"
 fi
 
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:please run this script with root user"
+    echo -e "$R ERROR:please run this script with root user $N"
     exit 1 #you can give other than 0
 else
-    echo "You are root user"
+    echo -e "$G You are root user $N"
 fi #fi means reverse of if , indicationg condition to end
 
 yum install mysql -y &>> $LOGFILE
