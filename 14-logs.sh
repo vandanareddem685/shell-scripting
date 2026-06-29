d30cb24fd430b943a68ffcc0de7ecd226b0e8a32
vandana <vandanareddem685@gmail.com>
2026-06-29 20:58:59 +0530

    Logs

---
+++ 14-logs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE= "/tmp/$0-$TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo "ERROR:$2 is failed"
+    else
+       echo "$2 is success"
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+   echo "ERROR:Please run this script with root access"
+else  
+   echo "You are root user"
+fi
+
+yum install mysql -y & >> LOGFILE
+VALIDATE $? "Installing mysql"
+
+yum install git -y & >> LOGFILE
+VALIDATE $? "Installing git"
