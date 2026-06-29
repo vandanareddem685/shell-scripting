6e0eeb13fd8153cb9b5edc564bffb6446416f52e
vandana <vandanareddem685@gmail.com>
2026-06-29 15:51:07 +0530

    install mysql

---
+++ 11-functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+ if [ $? -ne 0 ]
+ then
+    echo "Installation is failed"
+ else
+    echo "Installation is success"
+ fi
+}
+
+if [ $ID -ne 0 ]
+then
+   echo "ERROR:Please run this script with root access"
+   exit 1
+else
+   echo "You are root user"
+fi
+
+yum install mysql -y
+
+VALIDATE
