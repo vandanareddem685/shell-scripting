3e25f97f6e061f57174867dd6260e598b2d4e703
vandana <vandanareddem685@gmail.com>
2026-06-29 15:31:47 +0530

    install mysql

--- 10-install-mysql.sh
+++ 10-install-mysql.sh
@@ -14,6 +14,16 @@ yum install mysqll -y
 if [ $? -ne 0 ]
 then
    echo "ERROR:Installing MYSQL is failed"
+   exit 1
 else
    echo "Installing MYSQL is success"
+fi
+
+yum install git -y
+if [ $? -ne 0 ]
+then
+   echo "ERROR: INstalling git is failed"
+   exit1
+else
+   echo "Installing git is success"
 fi
