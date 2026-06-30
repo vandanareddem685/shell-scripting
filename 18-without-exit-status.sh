2347a4b3a91c0a1f23119660eca47a27f4f895d1
vandana <vandanareddem685@gmail.com>
2026-06-30 18:40:33 +0530

    install packages

---
+++ 18-without-exit-status.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+echo "Script started"
+
+ls -ltr
+
+touch example.txt
+
+echo "Before wrong command"
+
+lsfff
+
+echo "After wrong command"
+
+cd /tmp
+
+pwd
+
+cd /home/centos
+
+pwd
+
+echo "Script completed"
