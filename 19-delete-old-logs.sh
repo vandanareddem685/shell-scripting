017e93b4ba7c483840f791eae54af7c00a365ad3
vandana <vandanareddem685@gmail.com>
2026-06-30 20:14:41 +0530

    install packages

--- 19-delete-old-logs.sh
+++ 19-delete-old-logs.sh
@@ -16,6 +16,6 @@ FILES_TO_DELETE=$(find $SOURECE_DIR -type f -mtime +14 -name "*.logs")
 
 while IFS=read -r line
     do
-     echo "Deleting line:$line"
+     echo "Deleting line: $line"
      rm -rf $line
     done <<< $FILES_TO_DELETE
