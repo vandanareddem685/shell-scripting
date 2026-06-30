3e5f0ae269d933520f2473097a35f264cdd8b5e3
vandana <vandanareddem685@gmail.com>
2026-06-30 16:05:02 +0530

    install packages

--- 17-install-packages.sh
+++ 17-install-packages.sh
@@ -3,7 +3,22 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>>LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo -e "$R ERROR:$2 is failed $N"
+       exit 1
+    else
+       echo -e "$G $2 is success $N"
+    fi
 
 if [ $ID -ne 0 ]
 then
@@ -12,4 +27,14 @@ else
    echo "$G You are root user $N"
 fi
 
-echo "ALL args passed:$@"
+for package in $@
+do
+  yum list installed $package
+  if [ $? -ne 0 ]
+  then
+     yum install $package -y &>>LOGFILE
+     VALIDATE $? "Installing $package"
+  else
+    echo "$package already installed $Y SKIPPING $N"
+  fi
+done
