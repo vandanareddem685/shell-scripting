132b0537f9c79d9275e0265ed5a959189b2f6b03
vandana <vandanareddem685@gmail.com>
2026-06-30 15:28:45 +0530

    install packages

--- 17-install-packages.sh
+++ 17-install-packages.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $ID -ne 0 ]
 then
-   echo "ERROR:Please run this script with root access"
+   echo -e "$R ERROR:Please run this script with root access $N"
 else
-   echo "You are root user"
+   echo "$G You are root user $N"
 fi
 
 echo "ALL args passed:$@"
