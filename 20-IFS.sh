5b25871d6eef9649ad9ef14eea1c55b267db4991
vandana <vandanareddem685@gmail.com>
2026-07-01 16:38:55 +0530

    IFS

---
+++ 20-IFS.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file=/etc/passwd
+
+if[ ! f $file ]
+then
+   echo "Source file $file doesn't exists"
+fi
+
+while IFs=":" read -r username password user_id group_id user_fullname home_dir bash_path
+
+do
+  echo "Username is: $username"
+  echo "Passwd is: $password"
+  echo "user_id is: $user_id"
+done <<< $file
