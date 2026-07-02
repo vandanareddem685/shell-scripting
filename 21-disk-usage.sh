0f05987c5871669524da300560980c1eb43d83bf
vandana <vandanareddem685@gmail.com>
2026-07-02 12:32:40 +0530

    disk usage

--- 21-disk-usage.sh
+++ 21-disk-usage.sh
@@ -11,7 +11,7 @@ do
 
   if [ $usage -ge $DISK_THRESHOLD ]
   then
-        message+="High Disk Usage on $partition: $usage <br>"
+        message+="High Disk Usage on $partition: $usage\n"
     fi
 done <<< $DISK_USAGE
 
