0147694ef12e85bf43f2d0c2761672c4420d99d1
vandana <vandanareddem685@gmail.com>
2026-07-13 14:36:31 +0530

    opetargs

--- 22-greetings.sh
+++ 22-greetings.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+USAGE(){
+    echo "USGAE:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. (Optional). Default is Good Morning"
+    echo " -h, Display Help and exit"
+}
+
 while getopts "n:a:" opt
 do
     case $opt in
